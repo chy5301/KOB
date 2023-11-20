fbb813369e76c7fac885e07b8fe0404042aec938
chy5301 <conghaoyangbobby@gmail.com>
2023-11-21 00:16:42 +0800

    feat(web/.../GameMap):能够在地图内部创建指定数量的墙体并保证所有可通行区域连通
    没想到被一个200行的小模块卡了这么久，各种踩坑果然我还是太菜了
    - 问题规模足够小的时候，还非要优化优化效率的下场就是浪费两天时间，有时候暴力枚举就足够好了。当然也可能是我算法太菜了
    - 能复制粘贴代码的时候就不要手敲，因为抄错几个变量名而debug一晚上也太蠢了
    - 为了调试方便把部分逻辑拆出来单独实现，调试好再缝进去感觉是个不错的办法。但是换语言来实现导致缝回去的时候没法复制粘贴还是太离谱了

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,6 +1,26 @@
 import {KOBGameObject} from "@/assets/scripts/KOBGameObject";
 import {Wall} from "@/assets/scripts/Wall";
 
+// 坐标点
+class Point {
+    constructor(r, c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    equals(other) {
+        return this.r === other.r && this.c === other.c;
+    }
+}
+
+// 区块
+class Block {
+    constructor(r, c) {
+        this.parentPoint = new Point(r, c);
+        this.isWall = false;
+    }
+}
+
 export class GameMap extends KOBGameObject {
     constructor(ctx, parent) {
         super();
@@ -11,50 +31,145 @@ export class GameMap extends KOBGameObject {
         this.parent = parent;
         // 每个格子的单位长度（绝对距离）
         this.L = 0;
-        // 行数
-        this.rows = 13;
-        // 列数
-        this.cols = 13;
+        // 行列数
+        this.size = 16;
+        // 内部墙体半数（为了保证地图公平，以对角线为分界进行随机分配，所以只随机一般）
+        this.innerWallsCount = 48;
         // 墙体
         this.walls = [];
+        // 所有区块
+        this.blocks = [];
+        for (let r = 0; r < this.size; r++) {
+            this.blocks[r] = [];
+            for (let c = 0; c < this.size; c++)
+                this.blocks[r][c] = new Block(r, c);
+        }
+    }
+
+    // 判断point点对应的block的是否为并查集的根
+    isRoot(point) {
+        return this.blocks[point.r][point.c].parentPoint.equals(point);
+    }
+
+    // 找到start点对应的block在并查集中的根
+    findRoot(start) {
+        let point = new Point(start.r, start.c);
+        while (!this.isRoot(point)) {
+            point = this.blocks[point.r][point.c].parentPoint;
+        }
+        return point;
+    }
+
+    // 在并查集中合并point1点和point2点对应的block
+    merge(point1, point2) {
+        const rootPoint1 = this.findRoot(point1);
+        const rootPoint2 = this.findRoot(point2);
+
+        if (rootPoint1.equals(rootPoint2))
+            return;
+        this.blocks[rootPoint2.r][rootPoint2.c].parentPoint = rootPoint1;
     }
 
     // 创建障碍物
     createWalls() {
-        const g = [];
-        for (let r = 0; r < this.rows; r++) {
-            g[r] = [];
-            for (let c = 0; c < this.cols; c++)
-                g[r][c] = false;
+        // 初始化blocks
+        for (let r = 0; r < this.size; r++)
+            for (let c = 0; c < this.size; c++)
+                this.blocks[r][c] = new Block(r, c);
+        // 在地图边界设置墙
+        for (let r = 0; r < this.size; r++)
+            this.blocks[r][0].isWall = this.blocks[r][this.size - 1].isWall = true;
+        for (let c = 0; c < this.size; c++)
+            this.blocks[0][c].isWall = this.blocks[this.size - 1][c].isWall = true;
+
+        // 创建随机障碍物（关于对角线对称）
+        for (let i = 0; i < this.innerWallsCount / 2; i++)
+            for (let j = 0; j < 1000; j++) {
+                let r = Math.floor(Math.random() * this.size);
+                let c = Math.floor(Math.random() * this.size);
+                // 如果随机到已经是墙的位置，就重新随机
+                if (this.blocks[r][c].isWall || this.blocks[c][r].isWall)
+                    continue;
+                // 如果随机到边界，就重新随机
+                if (r === this.size - 2 && c === 1 || r === 1 && c === this.size - 2)
+                    continue;
+                this.blocks[r][c].isWall = this.blocks[c][r].isWall = true;
+                break;
+            }
+    }
+
+    floodFill(start) {
+        let oldPoint, newPoint;
+        const queue = [];
+        // 初始化方向
+        const orient = [0, -1, 0, 1, 0];
+        // 将起点加入队列
+        queue.push(new Point(start.r, start.c));
+
+        while (queue.length > 0) {
+            oldPoint = queue.shift();
+            // 遍历四个方向
+            for (let i = 0; i < 4; i++) {
+                const newR = oldPoint.r + orient[i];
+                const newC = oldPoint.c + orient[i + 1];
+                // 如果越界就跳过
+                if (newR < 0 || newC < 0 || newR === this.size || newC === this.size)
+                    continue;
+
+
+                newPoint = new Point(newR, newC);
+                // 如果是墙体或已经被合并，就跳过
+                if (this.blocks[newR][newC].isWall || !this.isRoot(newPoint))
+                    continue;
+
+                // 如果不是墙体，合并oldPoint和newPoint，并将newPoint加入队列
+                this.merge(oldPoint, newPoint);
+                queue.push(newPoint);
+            }
         }
 
-        // 在地图边界设置墙
-        for (let r = 0; r < this.rows; r++)
-            g[r][0] = g[r][this.cols - 1] = true;
-        for (let c = 0; c < this.cols; c++)
-            g[0][c] = g[this.rows - 1][c] = true;
+        // 统计根的数量
+        let rootCount = 0;
+        for (let i = 0; i < this.size; i++)
+            for (let j = 0; j < this.size; j++) {
+                if (this.blocks[i][j].isWall)
+                    continue;
+                if (this.isRoot(new Point(i, j)))
+                    rootCount++;
+            }
+        return rootCount;
+    }
 
-        // 渲染所有的墙
-        for (let r = 0; r < this.rows; r++)
-            for (let c = 0; c < this.cols; c++)
-                if (g[r][c])
+    // 将所有的墙添加到walls[]并初始化（到Object列表中）
+    pushWalls() {
+        for (let r = 0; r < this.size; r++)
+            for (let c = 0; c < this.size; c++)
+                if (this.blocks[r][c].isWall)
                     this.walls.push(new Wall(r, c, this));
     }
 
-
-    // 只执行一次
+    // 只在启动时执行一次
     start() {
-        this.createWalls();
+        // 以左下角为起点
+        const startPoint = new Point(this.size - 2, 1);
+        let rootCount;
+        // 创建墙体直到满足条件
+        do {
+            this.createWalls();
+            rootCount = this.floodFill(startPoint);
+        } while (rootCount > 1);
+
+        this.pushWalls();
     }
 
     // 更新Map的边长
     updateSize() {
         // 求格子单位长度
-        this.L = Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows);
+        this.L = Math.floor(Math.min(this.parent.clientWidth / this.size, this.parent.clientHeight / this.size));
         // 计算实际宽度
-        this.ctx.canvas.width = this.L * this.cols;
+        this.ctx.canvas.width = this.L * this.size;
         // 计算实际高度
-        this.ctx.canvas.height = this.L * this.rows;
+        this.ctx.canvas.height = this.L * this.size;
     }
 
     // 除了第一帧之外，每一帧执行一次
@@ -72,9 +187,9 @@ export class GameMap extends KOBGameObject {
         // 定义奇数格子的颜色
         const colorOdd = "#a2d149";
         // 循环绘制每一行的格子
-        for (let r = 0; r < this.rows; r++) {
+        for (let r = 0; r < this.size; r++) {
             // 循环绘制每一列的格子
-            for (let c = 0; c < this.cols; c++) {
+            for (let c = 0; c < this.size; c++) {
                 // 判断当前格子是偶数格子还是奇数格子并设置对应的颜色
                 if ((r + c) % 2 === 0) {
                     this.ctx.fillStyle = colorEven;
@@ -86,5 +201,4 @@ export class GameMap extends KOBGameObject {
             }
         }
     }
-
 }
