ecc8a73880b40539b9c9447e34b433ab23f9d681
chy5301 <conghaoyangbobby@gmail.com>
2024-01-27 22:36:13 +0800

    chore: 创建新的bot-running-system子模块，用于实现bot代码执行微服务

---
+++ backend-cloud/bot-running-system/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.kob.botrunningsystem"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-security
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.0")
+
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    implementation("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    // https://mvnrepository.com/artifact/org.jooq/joor-java-8
+    implementation("org.jooq:joor-java-8:0.9.15")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- backend-cloud/settings.gradle.kts
+++ backend-cloud/settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "backend-cloud"
 include("matching-system")
 include("backend")
+include("bot-running-system")
